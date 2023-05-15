cfddf2be3bead5bf0df64cc6c877b5c114a7f578
Eva Demmer <eva.demmer@gmail.com>
2023-05-15 13:56:50 +0200

    Initial commit

---
+++ frontend/src/pages/Error.jsx
@@ -0,0 +1,9 @@
+import "../main.scss";
+
+export default function Error() {
+  return (
+    <div className="error">
+      <h1>Error 404</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import "../main.scss";
+
+export default function Home() {
+  return <div className="home">Home</div>;
+}
